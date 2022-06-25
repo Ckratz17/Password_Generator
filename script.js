2b8250916848121928b918537409b85429073f0c
Chris Kratz <Ckratz17@gmail.com>
2022-06-25 19:57:40 -0400

    generatePassword defined in the console

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 // Assignment Code
-var letters =("abcdefghijklmnopqrstuvwxyz")//letter variables
-var numbers =(1234567890)//number variables
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate")
 
-  passwordText.value = password;
-
+function generatePassword () {
+    console.log("Ouch! That hurt!")
 }
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
 
