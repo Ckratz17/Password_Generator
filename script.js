6934f2c29d7face0d9e83b0f37b7e923ee53b835
Chris Kratz <Ckratz17@gmail.com>
2022-06-25 22:22:08 -0400

    UP AND RUNNING git status

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ var specialChar = ["!","@","#","$","%","&","*","_","-","+",]
 var lowerCase = ['a','B','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numbers = ['1','2','3','4','5','6','7','8','9','0'];
-
-// Write password to the #password input
-
 var generateBtn = document.querySelector("#generate")
+
+generateBtn.addEventListener("click", writePassword);
+
+
 function popup() {
     choice = [];
 
@@ -16,6 +17,7 @@ function popup() {
 
     if(isNaN(Length) || Length < 8 || Length > 128) { //alert if someone enters a character that is not a number 8-128
         alert("Character length has to be a number, 8 - 128 digits. Try agin.")
+        return false;
     }
 
     if(confirm("Do you want lowercase letters in your password?")) {
@@ -36,33 +38,22 @@ function popup() {
     return true;
 }
 
-function generatePassword () {
-    console.log("Ouch! That hurt!")
-   
-    //need to prompt the user for the password
-    //password length 8 < 128
-    //lowercase, upercase, numbers, special characters
-    //validate the input
-    //generate password based on criteria
-
-
-    //display generate password on page
-     return "Generated Password will go here!"
-}
-
 function writePassword() {
-
-    var password = generatePassword();
+    var usablePassword = popup()
     var passwordText = document.querySelector("#password");
 
-    passwordText.value = password
+    if(usablePassword){
+        var genPassword = generatePassword();
+        passwordText.value = genPassword;
+    }
 }
 
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
+function generatePassword () {
+    var password = " ";
+    for(var i = 0; i < Length; i++) {
+        var randomize = Math.floor(Math.random() * choice.length);
+        password = password + choice[randomize];
+    }
+     return password;
+}
